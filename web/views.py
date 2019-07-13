66e7169f530208aaf36dfde766ac04d68ffe82bb
José Alvarez <alvarej2@colpatria.com>
2019-07-13 02:16:25 -0500

    hola

--- web/views.py
+++ web/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Persona
 
-    # Create your views here.
+
+class IndexView(ListView):
+    model = Persona
